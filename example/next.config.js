6d25bb7e3b6985ff6ca40899dae3c7dd1fe7aae2
JongHakSeo <unqocn@gmail.com>
2021-08-06 11:09:39 +0900

    release: 1.1.0

--- example/next.config.js
+++ example/next.config.js
@@ -1,8 +1,11 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
   exportTrailingSlash: true,
   exportPathMap: function() {
     return {
       '/': { page: '/' }
     };
-  }
+  },
+  plugins:[new CleanWebpackPlugin],
 };
